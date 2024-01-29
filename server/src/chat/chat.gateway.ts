b96fc2d22ea21af8e4ab87f6f884c1f5eeb927c7
Gapur Kassym <gapur.kassym@gmail.com>
2024-01-29 22:58:45 +0200

    socket connect and disconnect

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -3,18 +3,16 @@ import {
   WebSocketGateway,
   WebSocketServer,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 import { AddMessageDto } from './dto/add-message.dto';
 
-@WebSocketGateway({
-  cors: {
-    origin: '*',
-  },
-})
-export class ChatGateway {
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
@@ -22,8 +20,16 @@ export class ChatGateway {
 
   @SubscribeMessage('chat')
   handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
-    this.logger.log(`Message received: ${payload}`);
+    this.logger.log(`Message received: ${payload.author} - ${payload.body}`);
     this.server.emit('chat', payload);
     return payload;
+  }
+
+  handleConnection(socket: Socket) {
+    this.logger.log(`Socket connected: ${socket.id}`);
+  }
+
+  handleDisconnect(socket: Socket) {
+    this.logger.log(`Socket disconnected: ${socket.id}`);
   }
 }
